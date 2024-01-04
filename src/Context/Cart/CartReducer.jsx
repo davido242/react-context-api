2800caca88303bba861bdf724dcb1f60d385d64d
Monday_David_Sarka <davidsarka242@gmail.com>
2024-01-04 21:44:11 +0100

    Added Calculations to sum up cart price and quantity

--- src/Context/Cart/CartReducer.jsx
+++ src/Context/Cart/CartReducer.jsx
@@ -6,3 +6,23 @@ import {
   CHECKOUT,
   CLEAR
 } from './CartTypes';
+
+// Export function to calculate the total price of the cart and the total quantity of the cart
+export const sumItems = (cartItems) => {
+  Storage(cartItems);
+
+  let itemCount = cartItems.reduce(
+    (total, product) => {
+      total + product.quantity, 0
+    }
+  );
+
+  let total = cartItems
+    .reduce((total, product) => total + product.price * product.quantity, 0)
+    .toFixed(2);
+
+    return { itemCount, total };
+};
+
+
+
